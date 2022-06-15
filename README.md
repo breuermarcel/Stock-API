ca8e75e6b3914738b80a98a6bec21256fe7a18fc
Marcel Breuer <breuer.marcel@outlook.com>
2022-06-15 19:01:55 +0200

    Optimized installation process

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 # Stock-API
 
 ## Requirements
+- npm
+- nodeJS
+- expressJS
 
 ## Installation
+```bash
+npm install
+```
+
+## Run
+```bash
+npm run start
+```
+
+Visit your browser [browserlink].
+
+[browserlink]: http://localhost:3000/
--- app.js
+++ app.js
@@ -4,9 +4,14 @@ const indexRouter = require("./routes/index");
 const stockRouter = require("./routes/stocks");
 
 const app = express();
+const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
+
 
 /**
  * Routes
